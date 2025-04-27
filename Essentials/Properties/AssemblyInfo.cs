62070b87ee42ddeaa45effb810d870bf61196830
SCP-SL-Nexus <nbnbgs@outlook.com>
2025-04-27 23:16:14 +0300

    1.14.1 ( I sereously )

---
+++ Essentials/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Essentials")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Essentials")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("190410cc-96d0-44d3-8f7b-388bd7a6e2c8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Essentials/commands/TPS.cs
@@ -0,0 +1,23 @@
+﻿using CommandSystem;
+using System;
+using LabApi.Features.Wrappers;
+
+namespace Essentials.Commands
+{
+    internal class TPS : ICommand
+    {
+        public static TPS Instance { get; } = new TPS();
+        public string Command { get; } = "TPS";
+        public string[] Aliases { get; } = { "t" };
+        public string Description { get; } = "Show you a server TPS";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            var config = Plugins.Singleton.Config;
+            var lang = config.language;
+            response = lang == "en" ? $"Server TPS: {Server.Tps}/{Server.MaxTps}" : $"TPS Сервера: {Server.Tps}/{Server.MaxTps}";
+            return false;
+        }
+    }
+}
+
